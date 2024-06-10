0477a71e982f7785a716b7ce3effffca145a7d3b
Sankalp-Space <sankalpshrivastav2001@gmail.com>
2024-06-10 09:31:18 +0530

    :)

--- sync_waitgroup/main.go
+++ sync_waitgroup/main.go
@@ -5,22 +5,22 @@ import (
 	"sync"
 )
 
-func worker(i int,wg *sync.WaitGroup ){
+func worker(i int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	fmt.Printf("worker %d started\n ",i)
+	fmt.Printf("worker %d started\n ", i)
 	//some taskis happening here
-	fmt.Printf("worker %d end\n ",i)
-	
+	fmt.Printf("worker %d end\n ", i)
+
 }
 
 func main() {
 	fmt.Println("Explore goroutine started")
 
 	var wg sync.WaitGroup
-	//start 3 worker goroutine 
-	for i:=1;i<=3;i++{
-		wg.Add(1)//increment the Waitgroup count
-		go worker(i,&wg)
+	//start 3 worker goroutine
+	for i := 1; i <= 3; i++ {
+		wg.Add(1) //increment the Waitgroup count
+		go worker(i, &wg)
 	}
 	wg.Wait()
 	fmt.Println("worker task completed")
